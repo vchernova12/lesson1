cc048f31af2da2ac603ac0675197d1cd30f5f0f3
vchernova12 <58112183+vchernova12@users.noreply.github.com>
2019-12-01 14:46:55 +0300

    Add files via upload

--- for_dict.py
+++ for_dict.py
@@ -5,11 +5,17 @@ students = [
   {'first_name': 'Маша'},
   {'first_name': 'Петя'},
 ]
-names = list()
+names = []
 print(type(names))
-for number_in_students in range(len(students)):
-    names += (students[number_in_students]['first_name'])
-    print(names)
-    print(students[number_in_students]['first_name'])
-
-    #for student in students[number_in_students]['first_name']:
+for student in range(len(students)):
+    names.append(f'{(students[student]["first_name"])}')
+print(names)
+uniq_names = []
+for student in range(len(students)):
+    print(uniq_names)
+    if student not in uniq_names:
+        uniq_names.append(f'{(students[student]["first_name"])}')
+        print(uniq_names)
+        #print names.count(students[student]["first_name"]))
+print(uniq_names)
+  
