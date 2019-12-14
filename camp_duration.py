7e379ae9477b03a0da1281d889bc5b52aeb81edc
vchernova12 <58112183+vchernova12@users.noreply.github.com>
2019-12-14 20:57:38 +0300

    Add files via upload

--- camp_duration.py
+++ camp_duration.py
@@ -1,19 +1,37 @@
 import calendar
 from datetime import datetime
-camp_begin= input('начало кампании')
-camp_end=input('конец кампании')
-#camp_begin = '12/23/2010'
-camp_begin= datetime.strptime(camp_begin, '%m/%d/%Y')
-camp_end= datetime.strptime(camp_end, '%m/%d/%Y')
-if camp_begin.month  != camp_end.month:
-    duration_month_1=int(calendar.monthrange(camp_begin.year,camp_begin.month)[1]) - camp_begin.day
-    duration_month_2=camp_end.day
-    duration_2_months=duration_month_1+duration_month_2
-    print(duration_2_months)
-else:
-    duration = (camp_end.day-camp_begin.day)
-    print(duration) 
 
 
-#datetime.datetime(2010, 12, 23, 0, 0)
-#print(camp_begin)
+def camp_duration():
+    camp_begin= input('начало кампании')
+    camp_end=input('конец кампании')
+    camp_begin= datetime.strptime(camp_begin, '%m/%d/%Y')
+    camp_end= datetime.strptime(camp_end, '%m/%d/%Y')
+    if camp_end.month - camp_begin.month == 1:
+        duration_first_month = int(calendar.monthrange(camp_begin.year,camp_begin.month)[1]) - camp_begin.day + 1
+        duration_last_month = camp_end.day
+        duration_2_months=duration_first_month + duration_last_month
+        print(duration_2_months)
+    elif camp_end.month-camp_begin.month > 1:
+        second_to_last_month=camp_begin.month
+        print(second_to_last_month)
+        duration_in_between=0
+        for months_in_camp  in range((camp_end.month-camp_begin.month-1)):
+            second_to_last_month +=1
+            duration_in_between += calendar.monthrange(camp_end.year,second_to_last_month)[1]
+            print(duration_in_between, second_to_last_month,camp_end.month)
+        duration_first_month = calendar.monthrange(camp_begin.year,camp_begin.month)[1] - camp_begin.day +1 
+        duration_last_month = camp_end.day
+        duration = duration_first_month+duration_last_month+duration_in_between
+        print(duration,duration_in_between)
+    elif camp_end.month==camp_begin.month:
+        duration = (camp_end.day-camp_begin.day)+1
+        print(duration) 
+    elif camp_end.month < camp_begin.month:
+        raise ValueError ("Пожалуйста, проверьте правильность дат в кампании. Окончание кампании запланировано раньше начала")
+
+
+if __name__ == "__main__":
+    camp_duration()
+
+
